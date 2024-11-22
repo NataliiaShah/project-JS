5b7630e3dd51b5929a4b6ed20055a8a5dd7d6d7c
AnnnaOs <preciousanutka@gmail.com>
2024-11-22 07:50:46 +0200

    Add js files

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/reviews.js';
+import '/js/mob-menu.js';
+import '/js/my-projects.js';
+import '/js/faq.js';
+import '/js/work-together.js';
+import '/js/modal.js';
+import '/js/switch-theme.js';
+import '/js/scroll-up-btn.js';
